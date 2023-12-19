4bf69c5e1bf90e5eacd82535a655fb5a6ae2ec78
Caleb Burrus <calebburrus@gmail.com>
2023-12-19 16:38:40 -0500

    create/update/delete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,40 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    render 'tasks/index'
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    render 'tasks/create' if @task.save
+  end
+
+  def mark_complete
+    @task = Task.find_by(id: params[:id])
+
+    render 'tasks/update' if @task&.update(completed: true)
+  end
+
+  def mark_active
+    @task = Task.find_by(id: params[:id])
+
+    render 'tasks/update' if @task&.update(completed: false)
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+
+    if @task&.destroy
+      render json: { success: true }
+    else
+      render json: { success: false }
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'homepage#index'
 
-  # Add routes below this line
-
-  # Add routes below above line
+  get 'tasks' => 'tasks#index'
+  post 'tasks' => 'tasks#create'
+  put 'tasks/:id/mark_complete' => 'tasks#mark_complete'
+  put 'tasks/:id/mark_active' => 'tasks#mark_active'
+  delete 'tasks/:id' => 'tasks#destroy'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
